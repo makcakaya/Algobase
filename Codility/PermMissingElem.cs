4b2f62abd9d82932f631ea1db146845acd36d5e0
Mert Akcakaya <amert.akcakaya@gmail.com>
2020-01-25 23:41:00 +0300

    PerMissingElem

---
+++ Codility/PermMissingElem.cs
@@ -0,0 +1,17 @@
+﻿namespace Codility
+{
+    public sealed class PermMissingElem
+    {
+        public static int Solution(int[] array)
+        {
+            var n = array.Length + 1L;
+            var total = n * (n + 1) / 2;
+            var sum = 0L;
+            for (var i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return (int)(total - sum);
+        }
+    }
+}
