c8d1b740b4e78bdd9e84da4e030fcb1b51f63c0c
Mert Akcakaya <amert.akcakaya@gmail.com>
2020-01-25 23:11:47 +0300

    FrogJmp

---
+++ Codility/FrogJmp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Codility
+{
+    public sealed class FrogJmp
+    {
+        public static int Solution(int position, int destination, int distance)
+        {
+            return (int)Math.Ceiling((double)(destination - position) / distance);
+        }
+    }
+}
