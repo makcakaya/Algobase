da065d14cbe57d92307717ac6b65695964066a17
Mert Akcakaya <amert.akcakaya@gmail.com>
2020-01-25 23:00:42 +0300

    OddOccurrencesInArray

---
+++ Codility/OddOccurrencesInArray.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Codility
+{
+    public sealed class OddOccurrencesInArray
+    {
+        public static int Solution(int[] array)
+        {
+            var set = new HashSet<int>();
+            foreach (var value in array)
+            {
+                if (set.Contains(value))
+                {
+                    set.Remove(value);
+                }
+                else
+                {
+                    set.Add(value);
+                }
+            }
+            return set.ToArray()[0];
+        }
+    }
+}
