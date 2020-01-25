dec769ee404094925ddcd433bc6059d618c5777e
Mert Akcakaya <amert.akcakaya@gmail.com>
2020-01-25 21:48:28 +0300

    CyclicRotation

---
+++ Codility/CyclicRotation.cs
@@ -0,0 +1,16 @@
+﻿namespace Codility
+{
+    public sealed class CyclicRotation
+    {
+        public int[] Solution(int[] array, int rotation)
+        {
+            var result = new int[array.Length];
+            for (var i = 0; i < array.Length; i++)
+            {
+                var dest = (i + rotation) % array.Length;
+                result[dest] = array[i];
+            }
+            return result;
+        }
+    }
+}
