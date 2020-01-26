e02e67f1eb01c2bfeb63c220f218f736c5af35b4
Mert Akcakaya <mert.akcakaya@osf-commerce.com>
2020-01-26 15:55:27 +0300

    TapeEquilibrium

---
+++ Codility/TapeEquilibrium.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Codility
+{
+    public sealed class TapeEquilibrium
+    {
+        public static int Solution(int[] array)
+        {
+            var right = array.Sum();
+            var left = 0;
+            var minDif = int.MaxValue;
+            for (var i = 0; i < array.Length - 1; i++)
+            {
+                left += array[i];
+                right -= array[i];
+                var dif = Math.Abs(left - right);
+                minDif = Math.Min(dif, minDif);
+            }
+            return minDif;
+        }
+    }
+}
