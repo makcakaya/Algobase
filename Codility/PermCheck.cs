9e757d39ce01496efcf0920754746c31698951a3
Mert Akcakaya <mert.akcakaya@osf-commerce.com>
2020-01-29 22:06:49 +0300

    PermCheck

---
+++ Codility/PermCheck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Codility
+{
+    public sealed class Perm
+    {
+        public static int Solution(int[] array)
+        {
+            Array.Sort(array);
+            var current = 1;
+            for (var i = 0; i < array.Length; i++)
+            {
+                if (array[i] != current++)
+                {
+                    return 0;
+                }
+            }
+            return 1;
+        }
+    }
+}
