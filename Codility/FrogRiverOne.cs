39ff0ff82bcf3352f84694fb0a0cd8edd8d7759c
Mert Akcakaya <mert.akcakaya@osf-commerce.com>
2020-01-29 21:46:39 +0300

    FrogRiverOne

---
+++ Codility/FrogRiverOne.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Codility
+{
+    public sealed class FrogRiverOne
+    {
+        public static int Solution(int distance, int[] leaves)
+        {
+            var missingSteps = new HashSet<int>();
+            for (var i = 1; i <= distance; i++)
+            {
+                missingSteps.Add(i);
+            }
+
+            for (var i = 0; i < leaves.Length; i++)
+            {
+                if (missingSteps.Contains(leaves[i]))
+                {
+                    missingSteps.Remove(leaves[i]);
+                }
+                if (missingSteps.Count == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
