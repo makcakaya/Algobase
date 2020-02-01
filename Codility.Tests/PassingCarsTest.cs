9c3c45be6c7f7d6b6abeaa6d1bcdac6e8b3e9a6e
Mert Akcakaya <amert.akcakaya@gmail.com>
2020-02-01 22:30:43 +0300

    PassingCars

---
+++ Codility.Tests/PassingCarsTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Codility.Tests
+{
+    public sealed class PassingCarsTest
+    {
+        [Fact]
+        public void Solution()
+        {
+            var input = new int[] { 0, 1, 0, 1, 1 };
+            var result = PassingCars.Solution(input);
+            Assert.Equal(5, result);
+        }
+    }
+}
---
+++ Codility/PassingCars.cs
@@ -0,0 +1,37 @@
+﻿namespace Codility
+{
+    public sealed class PassingCars
+    {
+        public static int Solution(int[] cars)
+        {
+            const int max = 1000000000;
+            var countZeros = 0;
+            var countPassed = 0;
+            var totalPassed = 0L;
+            for (var i = 0; i < cars.Length; i++)
+            {
+                var car = cars[i];
+                if (car == 0)
+                {
+                    totalPassed += countPassed * countZeros++;
+                    if (totalPassed > max || totalPassed < 0)
+                    {
+                        return -1;
+                    }
+                    countPassed = 0;
+                }
+                else
+                {
+                    countPassed++;
+                }
+            }
+
+            totalPassed += countPassed * countZeros;
+            if (totalPassed > max || totalPassed < 0)
+            {
+                return -1;
+            }
+            return (int)totalPassed;
+        }
+    }
+}
