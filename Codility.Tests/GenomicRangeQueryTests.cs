5cf967aa9b0ef6c0addba9d04c4f3fd07aba6a9a
Mert Akcakaya <mert.akcakaya@osf-commerce.com>
2020-02-07 09:44:32 +0300

    GenomicRangeQuery

---
+++ Codility.Tests/GenomicRangeQueryTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Codility.Tests
+{
+    public sealed class GenomicRangeQueryTests
+    {
+        [Fact]
+        public void Solution()
+        {
+            var result = GenomicRangeQuery.Solution("ACGTTAC", new int[] { 0 }, new int[] { 4 });
+            Assert.Equal(new int[] { 1 }, result);
+        }
+    }
+}
---
+++ Codility/GenomicRangeQuery.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Codility
+{
+    public sealed class GenomicRangeQuery
+    {
+        public static int[] Solution(string gen, int[] p, int[] q)
+        {
+            const int NucCount = 4;
+            var emptyRow = new int[] { 0, 0, 0, 0 };
+            var occurrences = new int[gen.Length][];
+            for (var i = 0; i < gen.Length; i++)
+            {
+                var nuc = gen[i];
+                var col = 0;
+                switch (nuc)
+                {
+                    case 'A':
+                        col = 0;
+                        break;
+
+                    case 'C':
+                        col = 1;
+                        break;
+
+                    case 'G':
+                        col = 2;
+                        break;
+
+                    case 'T':
+                        col = 3;
+                        break;
+                }
+                var prevRow = i == 0 ? new int[NucCount] : occurrences[i - 1];
+                occurrences[i] = new int[NucCount];
+                Array.Copy(prevRow, occurrences[i], prevRow.Length);
+                occurrences[i][col]++;
+            }
+
+            var result = new int[p.Length];
+            for (var i = 0; i < p.Length; i++)
+            {
+                var lowIndex = p[i] - 1;
+                var highIndex = q[i];
+                var lowRow = lowIndex < 0 ? emptyRow : occurrences[lowIndex];
+                var highRow = occurrences[highIndex];
+                for (var col = 0; col < NucCount; col++)
+                {
+                    if (highRow[col] > lowRow[col])
+                    {
+                        result[i] = col + 1;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
