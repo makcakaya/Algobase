b59cc33eff7bdd6d1744e4384bc2fd66b4cf047a
Mert Akcakaya <mert.akcakaya@osf-commerce.com>
2020-02-08 15:42:05 +0300

    MinAvgTwoSlice

---
+++ Codility.Tests/MinAvgTwoSliceTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Codility.Tests
+{
+    public sealed class MinAvgTwoSliceTest
+    {
+        [Fact]
+        public void Solution()
+        {
+            var result = MinAvgTwoSlice.Solution(new int[] { 4, 2, 2, 5, 1, 5, 8 });
+            Assert.Equal(1, result);
+        }
+    }
+}
---
+++ Codility/MinAvgTwoSlice.cs
@@ -0,0 +1,30 @@
+﻿namespace Codility
+{
+    public sealed class MinAvgTwoSlice
+    {
+        public static int Solution(int[] values)
+        {
+            var minAverage = float.MaxValue;
+            var minIndex = 0;
+            for (var i = 0; i < values.Length - 1; i++)
+            {
+                var average = (values[i] + values[i + 1]) / 2f;
+                if (average < minAverage)
+                {
+                    minIndex = i;
+                    minAverage = average;
+                }
+                if (i < values.Length - 2)
+                {
+                    var average3 = (values[i] + values[i + 1] + values[i + 2]) / 3f;
+                    if (average3 < minAverage)
+                    {
+                        minIndex = i;
+                        minAverage = average3;
+                    }
+                }
+            }
+            return minIndex;
+        }
+    }
+}
