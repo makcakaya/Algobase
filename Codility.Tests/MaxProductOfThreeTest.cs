09d92848e09e094e2495b389a2a34c236ebe09e1
Mert Akcakaya <mert.akcakaya@osf-commerce.com>
2020-02-15 21:51:49 +0300

    MaxProductOfThree

---
+++ Codility.Tests/MaxProductOfThreeTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public sealed class MaxProductOfThreeTest
+    {
+        [Fact]
+        public void Solution()
+        {
+            var data = new List<dynamic>
+            {
+                new {Values = new int[]{-10, 0, 0, 1, 4, 9, -8 }, Result = 720},
+                new {Values = new int[]{100, 0, 0, 1, 4, 9 }, Result = 3600},
+                new {Values = new int[]{-10, -20, -30, 0, 1, 4, 9, -8 }, Result = 5400},
+                new {Values = new int[]{-10, -20, -30, -4 }, Result = -800}
+            };
+
+            foreach (var d in data)
+            {
+                Assert.Equal(d.Result, MaxProductOfThree.Solution(d.Values));
+            }
+        }
+    }
+}
---
+++ Codility/MaxProductOfThree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Codility
+{
+    public sealed class MaxProductOfThree
+    {
+        public static int Solution(int[] values)
+        {
+            var n = values.Length;
+            if (n < 3)
+            {
+                return 0;
+            }
+
+            Array.Sort(values);
+            if (values[n - 1] < 0)
+            {
+                return values[n - 1] * values[n - 2] * values[n - 3];
+            }
+            else
+            {
+                var botTwoMult = values[0] * values[1];
+                var topTwoMult = values[n - 2] * values[n - 3];
+                var greatTwo = values[0] < 0 && botTwoMult > topTwoMult ? botTwoMult : topTwoMult;
+                return greatTwo * values[n - 1];
+            }
+        }
+    }
+}
