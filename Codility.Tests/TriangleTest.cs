93a27601475d5533817176e59800b4e3297c8da7
Mert Akcakaya <mert.akcakaya@osf-commerce.com>
2020-02-15 20:30:28 +0300

    Triangle

---
+++ Codility.Tests/TriangleTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public sealed class TriangleTest
+    {
+        [Fact]
+        public void Solution()
+        {
+            var data = new List<dynamic>()
+            {
+                new { Array = new int[3] { 0, 0, 0 }, Result = 0 },
+                new { Array = new int[3] { 1, 2, 2 }, Result = 1 },
+                new { Array = new int[2] { 0, 2 }, Result = 0 },
+                new { Array = new int[6] { 10, 2, 5, 1, 8, 20 }, Result = 1 },
+            };
+
+            foreach (var d in data)
+            {
+                Assert.Equal(d.Result, Triangle.Solution(d.Array));
+            }
+        }
+    }
+}
---
+++ Codility/Triangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Codility
+{
+    public sealed class Triangle
+    {
+        public static int Solution(int[] values)
+        {
+            if (values.Length < 3)
+            {
+                return 0;
+            }
+            Array.Sort(values);
+            for (var i = 0; i < values.Length - 2; i++)
+            {
+                if (values[i] > values[i + 2] - values[i + 1])
+                {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+    }
+}
