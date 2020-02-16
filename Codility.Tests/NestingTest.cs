d644f23b73fb139779920d171958fbca3ea29136
Mert Akcakaya <mert.akcakaya@osf-commerce.com>
2020-02-16 23:26:59 +0300

    Nesting

---
+++ Codility.Tests/NestingTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public sealed class NestingTest
+    {
+        [Fact]
+        public void Solution()
+        {
+            var data = new List<dynamic>
+            {
+                new {Value = "()()()", Result = 1},
+                new {Value = "", Result = 1},
+                new {Value = "()(())()", Result = 1},
+                new {Value = "((()))", Result = 1},
+                new {Value = "()((()))()()", Result = 1},
+                new {Value = "(()()", Result = 0},
+                new {Value = "(", Result = 0},
+                new {Value = "))", Result = 0},
+                new {Value = "())", Result = 0}
+            };
+            foreach (var d in data)
+            {
+                Assert.Equal(d.Result, Nesting.Solution(d.Value));
+            }
+        }
+    }
+}
---
+++ Codility/Nesting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Codility
+{
+    public sealed class Nesting
+    {
+        public static int Solution(string value)
+        {
+            var stack = new Stack<char>();
+            foreach (var c in value)
+            {
+                if (c == '(')
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        return 0;
+                    }
+                    stack.Pop();
+                }
+            }
+            return stack.Count == 0 ? 1 : 0;
+        }
+    }
+}
