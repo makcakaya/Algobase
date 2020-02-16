974852210518e49295f70459f5e50b435bd6dc74
Mert Akcakaya <mert.akcakaya@osf-commerce.com>
2020-02-16 21:58:04 +0300

    Distinct

---
+++ Codility.Tests/DistinctTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public sealed class DistinctTest
+    {
+        [Fact]
+        public void Solution()
+        {
+            var data = new List<dynamic>
+            {
+                new {Values = new int[] {-1, -4, 0, 1, -1, 0, 40, 100}, Result = 6 },
+                new {Values = new int[] {}, Result = 0 },
+                new {Values = new int[] {-1, -1, 0, -1, 0}, Result = 2 }
+            };
+
+            foreach (var d in data)
+            {
+                Assert.Equal(d.Result, Distinct.Solution(d.Values));
+            }
+        }
+    }
+}
---
+++ Codility/Distinct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Codility
+{
+    public sealed class Distinct
+    {
+        public static int Solution(int[] values)
+        {
+            Array.Sort(values);
+            var count = 0;
+            int? prev = null;
+            foreach (var value in values)
+            {
+                if (value != prev)
+                {
+                    count++;
+                    prev = value;
+                }
+            }
+            return count;
+        }
+    }
+}
