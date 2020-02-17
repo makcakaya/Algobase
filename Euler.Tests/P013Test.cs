9d555b783d8c6ed76dc96444b5f79eb0b019a35b
Mert Akcakaya <mert.akcakaya@osf-commerce.com>
2020-02-17 17:14:57 +0300

    Euler P013

---
+++ Euler.Tests/P013Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Euler.Tests
+{
+    [TestFixture]
+    public sealed class P013Test
+    {
+        [Test]
+        public void Solution()
+        {
+            var result = P013.Solve();
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Euler/P013.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace Euler
+{
+    public sealed class P013
+    {
+        public static string Solve()
+        {
+            const int digitPerTake = 10;
+            var inputLines = File.ReadAllLines(Path.Combine("Inputs", "P013Input.txt"));
+            var startIndex = (inputLines[0].Length);
+            long total = 0L;
+            for (var i = 0; i < 5; i++)
+            {
+                total = 0;
+                startIndex -= digitPerTake;
+                foreach (var line in inputLines)
+                {
+                    var take = line.Substring(startIndex, digitPerTake);
+                    var value = long.Parse(take);
+                    total += value;
+                }
+                var totalStr = total.ToString();
+                var remainder = totalStr.Substring(0, totalStr.Length - digitPerTake);
+                if (!string.IsNullOrEmpty(remainder))
+                {
+                    total += int.Parse(remainder);
+                }
+            }
+            return total.ToString().Substring(0, 10);
+        }
+    }
+}
