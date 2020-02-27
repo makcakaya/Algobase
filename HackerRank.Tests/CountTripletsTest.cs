d2d8f9a25f4c7ef2ddeed66768e4653311c7dbea
Mert Akcakaya <mert.akcakaya@osf-commerce.com>
2020-02-27 20:31:35 +0300

    HackerRank

---
+++ HackerRank.Tests/CountTripletsTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace HackerRank.Tests
+{
+    [TestFixture]
+    public sealed class CountTripletsTest
+    {
+        [Test]
+        public void Solution()
+        {
+            var data = new List<dynamic>
+            {
+                new {Values = new List<long>{1,2,4,5}, Ratio = 2, Result = 1},
+                new {Values = new List<long>{1,1,2,4}, Ratio = 2, Result = 2},
+                new {Values = new List<long>{1,2,2,2}, Ratio = 2, Result = 0 },
+                new {Values = new List<long>{4,2,2,4,4,1,1 }, Ratio =2, Result = 0 }
+            };
+            foreach (var d in data)
+            {
+                Assert.AreEqual(d.Result, CountTriplets.Solution(d.Values, d.Ratio));
+            }
+        }
+    }
+}
---
+++ HackerRank/CountTriplets.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace HackerRank
+{
+    public sealed class CountTriplets
+    {
+        public static long Solution(List<long> arr, long r)
+        {
+            var t2 = new Dictionary<long, long>();
+            var t3 = new Dictionary<long, long>();
+            var result = 0L;
+            foreach (var val in arr)
+            {
+                t3.TryGetValue(val, out var existing);
+                result += existing;
+                if (t2.ContainsKey(val))
+                {
+                    t3.TryGetValue(val * r, out var t3Existing);
+                    t3[val * r] = t3Existing + t2[val];
+                }
+                t2.TryGetValue(val * r, out var t2Existing);
+                t2[val * r] = t2Existing + 1;
+            }
+            return result;
+        }
+    }
+}
