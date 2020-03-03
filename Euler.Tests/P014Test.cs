6e27db4340d5185728d6dfa14f4c0b3b9f08fa04
Mert Akcakaya <mert.akcakaya@osf-commerce.com>
2020-03-03 15:28:25 +0300

    Euler P014

---
+++ Euler.Tests/P014Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Euler.Tests
+{
+    [TestFixture]
+    public sealed class P014Test
+    {
+        [Test]
+        public void Solve()
+        {
+            var result = P014.Solve();
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Euler/P014.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace Euler
+{
+    public sealed class P014
+    {
+        private readonly static int Limit = 1000000;
+
+        public static int Solve()
+        {
+            var maxSequenceCount = 0;
+            var maxStart = 0;
+            var processed = new HashSet<int>();
+            for (var i = Limit; i > 1; i--)
+            {
+                if (processed.Contains(i)) { continue; }
+                var count = Collatz(i, processed);
+                if (count > maxSequenceCount)
+                {
+                    maxSequenceCount = count;
+                    maxStart = i;
+                }
+            }
+            return maxStart;
+        }
+
+        public static int Collatz(long value, HashSet<int> processed)
+        {
+            var count = 0;
+            while (value != 1)
+            {
+                if (value <= Limit)
+                {
+                    processed.Add((int)value);
+                }
+                value = value % 2 == 0 ? (value / 2) : value * 3 + 1;
+                count++;
+            }
+            return count;
+        }
+    }
+}
