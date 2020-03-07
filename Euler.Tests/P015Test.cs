3ae02acd394e445a3498f907d38ae42cbeb2260c
Mert Akcakaya <mert.akcakaya@osf-commerce.com>
2020-03-07 18:58:29 +0300

    Euler P015

---
+++ Euler.Tests/P015Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Euler.Tests
+{
+    [TestFixture]
+    public sealed class P015Test
+    {
+        [Test]
+        public void Solve()
+        {
+            var count = P015.Solve(20);
+            Assert.AreEqual(137846528820, count);
+        }
+    }
+}
---
+++ Euler/P015.cs
@@ -0,0 +1,20 @@
+﻿namespace Euler
+{
+    public sealed class P015
+    {
+        public static long Solve(int n)
+        {
+            return CountPaths(n, 0, 0, new long[n, n]);
+        }
+
+        public static long CountPaths(long size, int row, int col, long[,] mem)
+        {
+            if (row == size || col == size) { return 1; }
+            if (mem[row, col] == 0)
+            {
+                mem[row, col] = CountPaths(size, row + 1, col, mem) + CountPaths(size, row, col + 1, mem);
+            }
+            return mem[row, col];
+        }
+    }
+}
