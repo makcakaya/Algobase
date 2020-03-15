8cd3dd0145f3be2353b756b6bb7d91cf7ca5c7b0
Mert Akcakaya <mert.akcakaya@osf-commerce.com>
2020-03-15 14:53:19 +0300

    HR ClimbingTheLeaderboard

---
+++ HackerRank.Tests/ClimbingTheLeaderboard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank.Tests
+{
+    public sealed class ClimbingTheLeaderboard
+    {
+        public static int[] Solution(int[] scores, int[] alice)
+        {
+            var board = new Dictionary<int, int>();
+            foreach (var s in scores)
+            {
+                board[s] = board.ContainsKey(s) ? board[s] + 1 : 1;
+            }
+
+            var ranks = new int[alice.Length];
+            var i = 0;
+            var higherThan = 0;
+            var keys = board.Keys.ToArray().OrderBy(k => k).ToArray();
+            for (var k = 0; i < ranks.Length;)
+            {
+                if (k >= keys.Length)
+                {
+                    ranks[i++] = keys.Length + 1 - higherThan;
+                    continue;
+                }
+
+                var score = keys[k];
+                if (alice[i] >= score)
+                {
+                    higherThan++;
+                    k++;
+                }
+                else
+                {
+                    ranks[i++] = keys.Length + 1 - higherThan;
+                }
+            }
+            return ranks;
+        }
+    }
+}
---
+++ HackerRank.Tests/ClimbingTheLeaderboardTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace HackerRank.Tests
+{
+    [TestFixture]
+    public sealed class ClimbingTheLeaderboardTest
+    {
+        [Test]
+        public void Solution()
+        {
+            var data = new List<dynamic>
+            {
+                //new {Scores = new int[]{10, 30, 20, 100}, Alice = new int[]{5, 15, 40, 50}, Result = new int[]{5, 4,2,2 } },
+                //new {Scores = new int[]{100, 100, 50, 40, 40, 20, 10}, Alice = new int[]{5, 25, 50, 120}, Result = new int[]{6,4,2,1} }
+                new {Scores = new int[]{1}, Alice = new int[]{1,1}, Result = new int[]{1,1} }
+            };
+
+            foreach (var d in data)
+            {
+                Assert.AreEqual(d.Result, ClimbingTheLeaderboard.Solution(d.Scores, d.Alice));
+            }
+        }
+    }
+}
