ddbbee16bec6bdf0c96ec5f6d8e5d8e289307c58
Mert Akcakaya <mert.akcakaya@osf-commerce.com>
2020-03-18 13:39:37 +0300

    HR ExtraLongFactorials

---
+++ HackerRank.Tests/ExtraLongFactorialsTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace HackerRank.Tests
+{
+    [TestFixture]
+    public sealed class ExtraLongFactorialsTest
+    {
+        [Test]
+        public void Solution()
+        {
+            var data = new List<dynamic>
+            {
+                new {N = 30, Result = "265252859812191058636308480000000" }
+            };
+
+            foreach (var d in data)
+            {
+                Assert.AreEqual(d.Result, ExtraLongFactorials.Solution(d.N));
+            }
+        }
+    }
+}
---
+++ HackerRank/ExtraLongFactorials.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace HackerRank
+{
+    public sealed class ExtraLongFactorials
+    {
+        public static string Solution(int n)
+        {
+            var result = new BigInteger(n);
+            for (var i = 1; i < n; i++)
+            {
+                result = BigInteger.Multiply(result, new BigInteger(n - i));
+            }
+            return result.ToString();
+        }
+    }
+}
